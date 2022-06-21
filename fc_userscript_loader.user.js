5d88df4efbe4c31e8145e388fc9674e4eb5d39cf
Darkroman <roman8707@yahoo.com>
2022-06-21 15:40:35 -0700

    Update fc_userscript_loader.user.js

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -3,19 +3,19 @@
 // @version        github-latest
 // @description    Userscript to load Frozen Cookies
 // @author         Icehawk78 / erbkaiser
-// @homepage       https://github.darkroman.com/FrozenCookies-testing/
+// @homepage       https://darkroman.github.io/FrozenCookies-testing/
 // @include        http://orteil.dashnet.org/cookieclicker/
 // @include        https://orteil.dashnet.org/cookieclicker/
-// @updateURL      https://github.darkroman.com/FrozenCookies-testing/fc_userscript_loader.user.js
-// @downloadURL    https://github.darkroman.com/FrozenCookies-testing/fc_userscript_loader.user.js
+// @updateURL      https://darkroman.github.io/FrozenCookies-testing/fc_userscript_loader.user.js
+// @downloadURL    https://darkroman.github.io/FrozenCookies-testing/fc_userscript_loader.user.js
 // ==/UserScript==
 
-// Source:    https://github.com/darkroman/FrozenCookies-testing/main/
+// Source:    https://github.com/darkroman/FrozenCookies-testing/
 // Github.io: https://darkroman.github.io/FrozenCookies-testing/
 var loadInterval = setInterval(function() {
     const Game = unsafeWindow.Game;
     if (Game && Game.ready) {
         clearInterval(loadInterval);
-        Game.LoadMod("https://github.darkroman.com/FrozenCookies-testing/frozen_cookies.js");
+        Game.LoadMod("https://darkroman.github.io/FrozenCookies-testing/frozen_cookies.js");
     }
 }, 1000);
